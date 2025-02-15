fdd18c999a11d71813f2d84bc4d75309943a1a98
Akshay <akshay@olvy.co>
2025-02-15 18:15:54 +0530

    fix - typescript errors in generateSvgMap

--- utils/process-svgs.ts
+++ utils/process-svgs.ts
@@ -6,14 +6,18 @@ const inputFolder = path.join(__dirname, 'svgs-min');
 const outputFile = path.join(__dirname, 'svgMap.ts');
 
 const GenerateSVGMap = () => {
-  const svgMap = {};
-
   // Read all files in the svgs-min folder
-  const files = fs.readdirSync(inputFolder).filter((file) => path.extname(file) === '.svg');
+  const files: string[] = fs.readdirSync(inputFolder).filter((file: string) => path.extname(file) === '.svg');
 
-  files.forEach((file, index) => {
-    const filePath = path.join(inputFolder, file);
-    const svgCode = fs.readFileSync(filePath, 'utf8');
+  interface SVGMap {
+    [key: number]: string;
+  }
+
+  const svgMap: SVGMap = {};
+
+  files.forEach((file: string, index: number) => {
+    const filePath: string = path.join(inputFolder, file);
+    const svgCode: string = fs.readFileSync(filePath, 'utf8');
 
     // Add SVG code to the map with key as index + 1
     svgMap[index + 1] = svgCode.trim();
